085c4ef45255ebd3d92e7f39ee8d9e7b10399769
Ze Mendes <zepedrosmendes@gmail.com>
2023-02-14 11:08:34 +0000

    Updated client and server files

--- src/client.py
+++ src/client.py
@@ -1,6 +1,7 @@
 """CD Chat client program"""
 import logging
 import sys
+import socket
 
 from .protocol import CDProto, CDProtoBadFormat
 
@@ -9,6 +10,9 @@ logging.basicConfig(filename=f"{sys.argv[0]}.log", level=logging.DEBUG)
 
 class Client:
     """Chat Client process."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', 2000))
+    sock.sendall('ola'.encode())
 
     def __init__(self, name: str = "Foo"):
         """Initializes chat client."""
--- src/server.py
+++ src/server.py
@@ -1,11 +1,18 @@
 """CD Chat server program."""
 import logging
+import socket
 
 logging.basicConfig(filename="server.log", level=logging.DEBUG)
 
-
 class Server:
     """Chat Server process."""
+    #                           INET                TCP
+    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criar socket
+    serversock.bind(('localhost', 2000)) # associa endereço
+    serversock.listen(5) # tamanho da fila de espera
+    conn, addr = serversock.accept() # servidor espera por clientes, o servidor bloqueia e só desbloqueia quando alguem se ligar
+    data = conn.recv(1000)
+    print(data.decode())
 
     def loop(self):
         """Loop indefinetely."""
