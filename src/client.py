3a2cc35405c6a08e3b261e562b7827586b5adc39
Ze Mendes <zepedrosmendes@gmail.com>
2023-02-15 16:18:16 +0000

    classes better defined for client and server, server selector added

--- src/client.py
+++ src/client.py
@@ -10,17 +10,19 @@ logging.basicConfig(filename=f"{sys.argv[0]}.log", level=logging.DEBUG)
 
 class Client:
     """Chat Client process."""
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('localhost', 2000))
-    sock.sendall('ola'.encode())
+    
+    # sock.sendall('ola'.encode())
 
     def __init__(self, name: str = "Foo"):
         """Initializes chat client."""
-        pass
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
     def connect(self):
         """Connect to chat server and setup stdin flags."""
-        pass
+        
+        self.sock.connect(('localhost', 2000))
 
     def loop(self):
         """Loop indefinetely."""
--- src/server.py
+++ src/server.py
@@ -1,19 +1,49 @@
 """CD Chat server program."""
 import logging
 import socket
+import selectors
 
 logging.basicConfig(filename="server.log", level=logging.DEBUG)
 
 class Server:
     """Chat Server process."""
-    #                           INET                TCP
-    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criar socket
-    serversock.bind(('localhost', 2000)) # associa endereço
-    serversock.listen(5) # tamanho da fila de espera
-    conn, addr = serversock.accept() # servidor espera por clientes, o servidor bloqueia e só desbloqueia quando alguem se ligar
-    data = conn.recv(1000)
-    print(data.decode())
+
+    def __init__(self):
+        #                           INET                TCP
+        self.serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criar socket
+        self.serversock.bind(('localhost', 2000)) # associa endereço
+        self.serversock.listen(5) # tamanho da fila de espera
+        self.sel = selectors.DefaultSelector() # selector
+        self.sel.register(self.serversock, selectors.EVENT_READ, self.accept) 
+        self.activeClients = [] # lista para guardar os clientes atualmente ativos para envio de mensagens
+        
+
+    def accept(self, sock, mask):
+        conn, addr = sock.accept()  # Should be ready # servidor espera por clientes, o servidor bloqueia e só desbloqueia quando alguem se ligar
+        print('accepted', conn, 'from', addr)
+        conn.setblocking(False)
+        self.sel.register(conn, selectors.EVENT_READ, self.read)
+    
+    def read(self, conn, mask):
+        data = conn.recv(1000)
+
+        if data:
+            print('echoing', repr(data), 'to', conn) # nao e para dar echo, e para enviar para os outros clients
+            conn.send(data)
+
+        else:
+            print('closing', conn)
+            self.sel.unregister(conn)
+            conn.close()
+
+    
+    # print(data.decode())
 
     def loop(self):
         """Loop indefinetely."""
 
+        while True:
+            events = self.sel.select()
+            for key, mask in events:
+                callback = key.data
+                callback(key.fileobj, mask)
